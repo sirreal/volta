297653752bf6526b93a10622e5e0899f7e8a33bc
charlespierce <charlespierce@users.noreply.github.com>
2020-12-19 00:21:12 +0000

    Publishing GitHub Pages

---
+++ main/volta_migrate/v3/config/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["_IMPL_DESERIALIZE_FOR_LegacyPackageConfig",""],["_IMPL_DESERIALIZE_FOR_LegacyPlatform",""],["_IMPL_DESERIALIZE_FOR_NodeVersion",""]],"struct":[["LegacyPackageConfig",""],["LegacyPlatform",""],["NodeVersion",""]]});
